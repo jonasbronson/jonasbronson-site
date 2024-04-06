4010fa6cf612519ae1ca773027fd881185f0d122
Jonas Bronson <jonasbronson@ksu.edu>
2024-04-06 17:14:02 -0500

    work on home page

--- jonasbronson-site/src/Header.tsx
+++ jonasbronson-site/src/Header.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Logo from "./assets/logo.png";
+import { Link } from "react-router-dom";
 
 export default function Header(){
     return(
         <>
             <div className="header">
-                <img src={Logo} alt="Jonas." className="headerLogo"/>
-                
+                <Link to="" style={{height: "100%"}}><img src={Logo} alt="Jonas." className="headerLogo"></img></Link>
+                <div className="headerLinksContainer">
+                    <Link to="/Video" className="headerLinks">Video Portfolio</Link>
+                    <Link to="/Code" className="headerLinks">Coding Portfolio</Link>
+                </div>
             </div>
         </>
     );
--- jonasbronson-site/src/pages/Home.tsx
+++ jonasbronson-site/src/pages/Home.tsx
@@ -4,9 +4,29 @@ import { Link } from "react-router-dom";
 export default function Home(){
     return(
         <>
-            <div style={{display: "flex", justifyContent: "center"}}>
-                <button type="button" className="button" style={{marginRight: "1em"}}><Link to="/Code">Coding</Link></button>
-                <button type="button" className="button"><Link to="/Video">Video</Link></button>
+            <div style={{margin: "4em"}}>
+                <div className="homeFront">
+                    <h1 className="homeTitle">Hi, I'm <span>Jonas</span>.</h1>
+                    <h3 className="homeSubTitle">Computer Science/Cybersecurity Student<br></br>Hobbyist Videographer/Video Editor</h3>
+                </div>
+                <div style={{marginBottom: "15%"}}>
+                    <h2 className="homeSpecialHeader">I Specialize In</h2>
+                    <h3 className="homeSpecialList">Software Development <br/>
+                        Web Development <br/>
+                        Graphic Design <br/> 
+                        Video Editing <br/>
+                        Videography <br/>
+                        <span>and so on...</span></h3>
+                </div>
+                <div style={{marginBottom: "15%"}}>
+                    <h2 className="homePortfolio">See my portfolios</h2>
+                    <div style={{display: "flex", justifyContent: "center"}}>
+                        <button type="button" className="button" style={{marginRight: "1em"}}><Link to="/Code">Coding</Link></button>
+                        <button type="button" className="button"><Link to="/Video">Video</Link></button>
+                    </div>
+                    <h2 className="homePortfolio">... or scroll to find out more about me</h2>
+                </div>
+                    
             </div>
             
 
