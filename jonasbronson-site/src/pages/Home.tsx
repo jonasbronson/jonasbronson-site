f248e1cbebd098a35ccbeadb9fdedc505a405fdb
Jonas Bronson <jonasbronson@ksu.edu>
2024-04-06 20:52:53 -0500

    more updates to home

--- jonasbronson-site/src/pages/Home.tsx
+++ jonasbronson-site/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ProfileNature from "../assets/profileNature.jpg";
 
 export default function Home(){
     return(
         <>
-            <div style={{margin: "4em"}}>
+            <div style={{margin: "10%"}}>
                 <div className="homeFront">
                     <h1 className="homeTitle">Hi, I'm <span>Jonas</span>.</h1>
                     <h3 className="homeSubTitle">Computer Science/Cybersecurity Student<br></br>Hobbyist Videographer/Video Editor</h3>
@@ -21,15 +22,44 @@ export default function Home(){
                 <div style={{marginBottom: "15%"}}>
                     <h2 className="homePortfolio">See my portfolios</h2>
                     <div style={{display: "flex", justifyContent: "center"}}>
-                        <button type="button" className="button" style={{marginRight: "1em"}}><Link to="/Code">Coding</Link></button>
-                        <button type="button" className="button"><Link to="/Video">Video</Link></button>
+                        <button type="button" className="homeButton" style={{marginRight: "1em"}}><Link to="/Code">Coding</Link></button>
+                        <button type="button" className="homeButton"><Link to="/Video">Video</Link></button>
                     </div>
                     <h2 className="homePortfolio">... or scroll to find out more about me</h2>
                 </div>
-                    
-            </div>
-            
+                <div style={{marginBottom: "15%"}}>
+                    <h2 className="homeHeader">About Me</h2>
+                    <div style={{display: "flex"}}>
+                        <img src={ProfileNature} className="homeAboutPic" />
+                        <p className="homeAboutText">Hi, I'm Jonas Bronson, a college student majoring in computer science and cybersecurity at Kansas State 
+                            University. I've always had a dual passion in both video production and anything computer related since 
+                            my middle school years. <br /> <br />
 
+                            I took my first video production course at Shawnee Mission Northwest high school (SMNW) in 2019. During 
+                            my years there, I completed all of the video production courses and became an Executive Producer for the 
+                            advanced video production class' video program, KUGR. While in KUGR, I directed and produced videos, ran 
+                            productions, live-streamed, and did jobs for the school district. In addition, I also do my own work for 
+                            clients and personal projects. My video production work has won national awards in 2022 and 2023 from the 
+                            Journalism Education Association (JEA). I currently work for Wildcat Watch at Kansas State University as 
+                            a Student Organization Video Producer where I manage others, film and edit videos. <br /><br />
+
+                            I have an extensive background in computer programming, cybersecurity, game design, and graphic design. 
+                            During my years in high school, I completed all of the computer science courses offered. Moreover, I took 
+                            classes at the Shawnee Mission School District Center for Academic Achievement for both cybersecurity and 
+                            game design. I have designed websites including my mom's and the one you are looking at right now! 
+                            Currently, I am working through the Kansas State University curriculum for the Computer Science + 
+                            Cybersecurity Bachelor's degree. 
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <h2 className="homeHeader">Contact Me</h2>
+                    <p className="homeContactText">I'd love to hear from you. Contact me with questions, possible opportunities, or anything inbetween.</p>
+                </div>
+                <div>
+                    
+                </div> 
+            </div>
         </>
     );
 }
