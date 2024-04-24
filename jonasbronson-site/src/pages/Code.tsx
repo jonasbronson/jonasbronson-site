fb258df6dd2cb34191a6e44bab0ef6207bdebc01
Jonas Bronson <jonasbronson@ksu.edu>
2024-04-24 00:40:11 -0500

    start on coding portfolio

--- jonasbronson-site/src/pages/Code.tsx
+++ jonasbronson-site/src/pages/Code.tsx
@@ -3,7 +3,12 @@ import React from "react";
 export default function Code(){
     return(
         <>
-        
+            <div style={{margin: "10%"}}>
+                <h1 className="videoTitle"><span>Coding</span> Portfolio</h1>
+                <div style={{marginBottom: "15%"}}>
+                    <h2 className="genH2">My Work</h2>
+                </div>
+            </div>
         </>
     );
 }
